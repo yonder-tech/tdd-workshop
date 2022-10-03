aee5909495baf4af967a4939d6e186dd18972351
Harry Ford <harryford@YonderHarryFord.localdomain>
2022-10-03 13:31:39 +0100

    Strip back Fibonnaci

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,5 @@
+public class Fibonacci {
+    int calculate(int index) {
+            return 1;
+    }
+}
--- test/java/FibonacciTest.java
+++ test/java/FibonacciTest.java
@@ -11,40 +11,4 @@ public class FibonacciTest {
 
         assertEquals(expectedResult, fibonacci.calculate(testIndex));
     }
-
-    @Test
-    public void calculate2() {
-        Fibonacci fibonacci = new Fibonacci();
-        int testIndex = 2;
-        int expectedResult = 2;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
-    }
-
-    @Test
-    public void calculate3() {
-        Fibonacci fibonacci = new Fibonacci();
-        int testIndex = 3;
-        int expectedResult = 3;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
-    }
-
-    @Test
-    public void calculate4() {
-        Fibonacci fibonacci = new Fibonacci();
-        int testIndex = 4;
-        int expectedResult = 5;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
-    }
-
-    @Test
-    public void calculate6() {
-        Fibonacci fibonacci = new Fibonacci();
-        int testIndex = 6;
-        int expectedResult = 13;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
-    }
 }
